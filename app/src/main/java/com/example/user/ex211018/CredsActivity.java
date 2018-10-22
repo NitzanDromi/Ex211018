b39978e46456659281efef27e508cb18f9a5b759
NitzanDromi <nd0389@bs.amalnet.k12.il>
2018-10-22 15:15:25 +0300

    333

--- app/src/main/java/com/example/user/ex211018/CredsActivity.java
+++ app/src/main/java/com/example/user/ex211018/CredsActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.ex211018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CredsActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class CredsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creds);
+    }
+
+    public void return1(View view) {
+        Intent t=new Intent(this, CredsActivity.class);
+        startActivity(t);
     }
 }
